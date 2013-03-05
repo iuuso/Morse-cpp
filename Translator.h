43be0b6ad55e2faae1a489a9d0cd165893072889
iuuso <juuso.karlstrom@gmail.com>
2013-03-05 15:37:38 +0200

    Removed garbage.

--- Translator.h
+++ Translator.h
@@ -4,27 +4,6 @@
  #ifndef TRANSLATOR_H
  #define TRANSLATOR_H
 
-/******************LETTERS AND NUMBERS******************/
-//#define A='.-'
-
-/*
- class Translation
- {
- 		public:
- 			Translation();
- 			Translation( const std::string &user_input,
- 						 const std::string &translated);
- 			virtual ~Translation();
-
-		//Getters
-		const std::string& getInput(); const;
-
-		private:
-			std::string user_input;
-			std::string translated;
-
- }; */
-
  /*
   * Translation algorithm step-by-step
   * 1. Input string, i.e. "Python"
